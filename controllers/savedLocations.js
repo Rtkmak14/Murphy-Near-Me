6a13e1be106b60c883a2b2b8bef5b39338af334f
Jonathan <jonathanallen105@gmail.com>
2025-09-29 16:28:27 -0400

    completed show route.

--- controllers/savedLocations.js
+++ controllers/savedLocations.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const User = require('../models/user');
+const Location = require('../model/location')
 
 const verifyToken = require('../middleware/verify-token');
 
-router.get('/:savedLocation', verifyToken, (req, res) => {
+router.get('/:savedLocationId', verifyToken, async (req, res) => {
     try {
+        if (req.user._id !== req.params.userId){
+            return res.status(403).json({ err: "Unauthorized"});
+        }
 
+        const location = await Location.findById(req.params.savedLocationId)
+        
+        return res.status(200).json({ location })
     } catch (err) {
         console.log(err)
     }
