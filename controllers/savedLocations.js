094e6cba91bac52fc0d1dec752a63edde27d508d
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-29 16:59:12 -0500

    Add saved locations route and model

--- controllers/savedLocations.js
+++ controllers/savedLocations.js
@@ -2,9 +2,34 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const User = require('../models/user');
-const Location = require('../model/location')
+const Location = require('../models/location')
 
 const verifyToken = require('../middleware/verify-token');
+
+
+router.get("/", verifyToken, async (req,res)=> {
+    try {
+        const locations = await Location.find({owner: req.params.userId}).populate("owner")
+        res.status(201).json({locations})
+    }
+
+    catch (err) {
+        res.status(500).json({err:err.message})
+    }
+})
+
+router.post("/", verifyToken, async (req,res)=> {
+    try {
+        req.body.owner = req.user._id
+        const location = await Location.create(req.body)
+        location._doc.owner = req.user
+        res.status(201).json({location})
+    }
+
+    catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+})
 
 router.get('/:savedLocationId', verifyToken, async (req, res) => {
     try {
